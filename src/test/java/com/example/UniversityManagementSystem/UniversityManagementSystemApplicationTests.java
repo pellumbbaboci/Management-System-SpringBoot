22d9782d697a9b6cde5bad8ff32209f43a9f2c16
Pellumbengineer <pellumbbaboci@gmail.com>
2020-07-30 22:24:16 +0300

    Initialization

---
+++ src/test/java/com/example/UniversityManagementSystem/UniversityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UniversityManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
